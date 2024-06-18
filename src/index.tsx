85b36f404e7cc506788dbed2c85cec5804eb89d1
sushantsharmaa <sushant.x01@gmail.com>
2024-06-18 09:06:42 +0530

    redux-setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import thunk from 'redux-thunk';
+import { reducers } from './reducers';
+import { App } from './components/App';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { createStore, applyMiddleware } from 'redux';
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el!);
 
-interface AppProps {
-  color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-class App extends React.Component<AppProps> {
-  render() {
-    return <div>Hi there {this.props.color}</div>;
-  }
-}
-
-root.render(<App color="red" />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
